c4e1af2c1596cb97018da0554a2bb43c51e533d2
masadi <masadi.com@gmail.com>
2020-12-27 20:17:41 +0700

    update

--- app/Http/Controllers/EdsController.php
+++ app/Http/Controllers/EdsController.php
@@ -13,40 +13,10 @@ use Artisan;
 class EdsController extends Controller
 {
     public function index(Request $request){
-        $npsn = [
-            '20527178',
-            '20552093',
-            '20552094',
-            '20566321',
-            '20552101',
-            '69757252',
-            '20577814',
-            '20566538',
-            '20537396',
-            '20575281',
-            '20566322',
-            '20552129',
-            '69943135',
-            '69822425',
-            '20527169',
-            '20537411',
-            '20566541',
-            '20537415',
-            '20566545',
-            '69954356',
-            '20537419',
-            '20537399',
-            '20579411',
-            '69788145',
-            '20574521',
-            '20574533',
-            '20577092',
-            '20577070',
-        ];
         $sekolah = Sekolah::first();
         $get_sekolah = NULL;
         if($sekolah){
-            if(in_array($sekolah->npsn, $npsn)){
+            if(in_array($sekolah->npsn, config('npsn'))){
                 $get_sekolah = $sekolah;
             }
         }
---
+++ config/npsn.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    '20527178',
+    '20552093',
+    '20552094',
+    '20566321',
+    '20552101',
+    '69757252',
+    '20577814',
+    '20566538',
+    '20537396',
+    '20575281',
+    '20566322',
+    '20552129',
+    '69943135',
+    '69822425',
+    '20527169',
+    '20537411',
+    '20566541',
+    '20537415',
+    '20566545',
+    '69954356',
+    '20537419',
+    '20537399',
+    '20579411',
+    '69788145',
+    '20574521',
+    '20574533',
+    '20577092',
+    '20577070',
+
+];
